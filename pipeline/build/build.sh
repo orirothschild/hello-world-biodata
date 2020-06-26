f394737a83a1da8d1700b8cb2a6de91485b0c389
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-06-26 14:24:58 +0000

    fixed build stage

--- pipeline/build/build.sh
+++ pipeline/build/build.sh
@@ -3,5 +3,5 @@
 echo "***********************"
 echo "***build both images***"
 echo "***********************"
-docker build -t orirothschild/hello-world -f ./docker-ruby-hello-world ./docker-ruby-hello-world
-docker build -t orirothschild/hello-world -f ./docker-ruby-howdy-world ./docker-ruby-howdy-world
+docker build -t orirothschild/hello-world -f ./docker-ruby-hello-world/Dockerfile ./docker-ruby-hello-world
+docker build -t orirothschild/hello-world -f ./docker-ruby-howdy-world/Dockerfile ./docker-ruby-howdy-world
