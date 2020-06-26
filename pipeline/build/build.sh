7c17d2ca753f0de52e49a26e0e5c68bd789ece18
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-06-26 14:47:43 +0000

    webhook test

--- pipeline/build/build.sh
+++ pipeline/build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "***********************"
-echo "***build both images***"
-echo "***********************"
+echo "*************************"
+echo "****build both images****"
+echo "*************************"
 docker build -t orirothschild/hello-world -f ./docker-ruby-hello-world/Dockerfile ./docker-ruby-hello-world
 docker build -t orirothschild/howdy-world -f ./docker-ruby-howdy-world/Dockerfile ./docker-ruby-howdy-world
