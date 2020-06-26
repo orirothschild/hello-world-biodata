44dd5c300ef4bfd8d0b837511a3484dcb245f313
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-06-26 14:46:36 +0000

    added push stage to pipeline

--- pipeline/build/build.sh
+++ pipeline/build/build.sh
@@ -4,4 +4,4 @@ echo "***********************"
 echo "***build both images***"
 echo "***********************"
 docker build -t orirothschild/hello-world -f ./docker-ruby-hello-world/Dockerfile ./docker-ruby-hello-world
-docker build -t orirothschild/hello-world -f ./docker-ruby-howdy-world/Dockerfile ./docker-ruby-howdy-world
+docker build -t orirothschild/howdy-world -f ./docker-ruby-howdy-world/Dockerfile ./docker-ruby-howdy-world
